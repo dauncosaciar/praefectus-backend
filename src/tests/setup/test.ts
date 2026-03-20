adeca7d183d3769321981a919cad3dde1ebf45ce
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2026-03-20 12:08:22 -0300

    add a test setup file with methods for connect and disconnect of mongodb test database

---
+++ src/tests/setup/test.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectTestDatabase = async () => {
+  await mongoose.connect(process.env.DATABASE_URL);
+};
+
+export const disconnectTestDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+};
